66f8c7ae85db16577191a87c804acdeca42a9862
SaarChaffee <1240148332@qq.com>
2021-06-06 03:04:13 +0800

    new user with anti-injection attack

--- src/JDBC/DaoPlus.java
+++ src/JDBC/DaoPlus.java
@@ -6,9 +6,15 @@
  */
 package JDBC;
 
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DaoPlus {
-    /**TODO
-     * 新建用户
+    /**
+     * TODO
+     * 新建用户√
      * 用户登录
      * 读取用户数据
      * 查看好友
@@ -16,8 +22,39 @@ public class DaoPlus {
      * 删除好友
      * 商店看余额和没有的卡片
      * 氪金
-     * 抽卡
+     * 卡片增加
      * 查询卡片
      * 胜负场数及举报查询
-     * **/
+     *
+     * @return
+     */
+    public static int getNewUid() {
+        return new SecureRandom().nextInt( 999999999 );
+    }
+
+    public static int NewUser( String Account, String PassWord, int PhoneNumber ) {
+        int result = 0;
+        try{
+            Connection conn = ( Connection ) JDBC.getStat();
+            String str1 = "insert into AccountDate " + "value('?','?','?','?')";
+            PreparedStatement pre1 = conn.prepareStatement( str1 );
+            pre1.setInt( 1, getNewUid() );
+            pre1.setString( 2, Account );
+            pre1.setString( 3, PassWord );
+            pre1.setInt( 4, PhoneNumber );
+            result += pre1.executeUpdate();
+            String str2 = "insert into UserData " + "value('?','?','1','0','0','0','0','0',null)";
+            PreparedStatement pre2 = conn.prepareStatement( str2 );
+            pre2.setInt( 1, getNewUid() );
+            pre2.setString( 2, Account );
+            result += pre2.executeUpdate();
+            String str3 = "insert into CardColle " + "value('?')";
+            PreparedStatement pre3 = conn.prepareStatement( str3 );
+            pre3.setInt( 1, getNewUid() );
+            result += pre3.executeUpdate();
+        }catch( SQLException throwables ){
+            throwables.printStackTrace();
+        }
+        return result;
+    }
 }
