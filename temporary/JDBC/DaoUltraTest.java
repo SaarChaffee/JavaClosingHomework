da2d9413cc16e35da394eb7828525ae96e76c011
SaarChaffee <1240148332@qq.com>
2021-06-07 22:49:10 +0800

    start DaoUltra test

---
+++ temporary/JDBC/DaoUltraTest.java
@@ -0,0 +1,34 @@
+package JDBC;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Name: JavaClosingHomework
+ * @Author: SaarChaffee
+ * @Code: GBK
+ * @Date: Created in 2021 2021/6/7
+ */
+public class DaoUltraTest {
+
+    @Test
+    public void addFriend() {
+    }
+
+    @Test
+    public void deleteFriend() {
+    }
+
+    @Test
+    public void userWin() {
+    }
+
+    @Test
+    public void userLost() {
+    }
+
+    @Test
+    public void reported() {
+    }
+}
