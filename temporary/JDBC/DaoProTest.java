4409cd6e11288d3be0059d80e9584dbbaeb32e09
SaarChaffee <1240148332@qq.com>
2021-06-07 22:45:12 +0800

    finish recharge test

--- temporary/JDBC/DaoProTest.java
+++ temporary/JDBC/DaoProTest.java
@@ -14,17 +14,21 @@ public class DaoProTest {
 
     @Test
     public void isFriend() {
-        System.out.println(DaoPro.isFriend( 0,53097 ));
-        System.out.println(DaoPro.isFriend( 53097, 686838635));
+        System.out.println( DaoPro.isFriend( 0, 53097 ) );
+        System.out.println( DaoPro.isFriend( 53097, 686838635 ) );
     }
 
     @Test
     public void isRedemptionUsed() {
-        System.out.println(DaoPro.isRedemptionUsed( "313" ));
+        System.out.println( DaoPro.isRedemptionUsed( "313" ) );
     }
 
     @Test
     public void recharge() {
+        int result = 0;
+        int uid = 686838635;
+        String code = "yq1308301";
+        System.out.println( DaoPro.recharge( uid, code ) );
     }
 
     @Test
