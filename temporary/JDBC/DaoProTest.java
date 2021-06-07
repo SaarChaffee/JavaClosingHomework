21bb227695b7518d0774eb679f2012629e98980d
SaarChaffee <1240148332@qq.com>
2021-06-07 20:04:06 +0800

    finish isFriend test

---
+++ temporary/JDBC/DaoProTest.java
@@ -0,0 +1,41 @@
+package JDBC;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Name: JavaClosingHomework
+ * @Author: SaarChaffee
+ * @Code: GBK
+ * @Date: Created in 2021 2021/6/7
+ */
+public class DaoProTest {
+
+    @Test
+    public void isFriend() {
+        System.out.println(DaoPro.isFriend( 0,53097 ));
+        System.out.println(DaoPro.isFriend( 53097, 686838635));
+    }
+
+    @Test
+    public void isRedemptionUsed() {
+
+    }
+
+    @Test
+    public void recharge() {
+    }
+
+    @Test
+    public void getWin() {
+    }
+
+    @Test
+    public void getLost() {
+    }
+
+    @Test
+    public void getReport() {
+    }
+}
