4760b109c088e5c612b1c3bd72a4138c0a79ea7a
SaarChaffee <1240148332@qq.com>
2021-06-07 10:16:29 +0800

    finish NewUser() test

--- temporary/JDBC/DaoBaseTest.java
+++ temporary/JDBC/DaoBaseTest.java
@@ -15,8 +15,8 @@ public class DaoBaseTest {
 
     @Test
     public void update() {
-//        String str = "INSERT INTO dbo.AccountData(UserUid,Account,PassWord,PhoneNumber)VALUES(1,1,1,1)";
-        String str = "delete from AccountData where UserUid = '1'";
+        String str = "INSERT INTO AccountData VALUES(1,1,1,1)";
+//        String str = "delete from AccountData where UserUid = '1'";
         System.out.println( DaoBase.Update( str ) );
     }
 
